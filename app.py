e4f8da46b1074eff9e9026dbd5d7b651efb9a159
matire4 <remati6870@gmail.com>
2025-03-13 10:27:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -34,4 +34,4 @@ app.register_blueprint(conciliacion_bp)
 app.register_blueprint(extractos_bp)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
